16e7f5446d80f502a5020bb432ecad87eb79ffb6
geekestLi <3162853966@qq.com>
2021-06-27 18:21:40 +0800

    solve bug

--- src/routes/crawl.js
+++ src/routes/crawl.js
@@ -5,8 +5,8 @@ const crawl = require('../../common/crawler.js').default.craw
 
 router.post('/crawlurl',async function(req,res){
 	let result = ''
-	if(req.body.url){result = await crawl([req.body])}
-	else{result = await crawl(JSON.parse(req.body.data))}
+	if(req.body.url){result = await crawl([req.body],1)}
+	else{result = await crawl(JSON.parse(req.body.data),1)}
 	res.send(result)
 })
 
