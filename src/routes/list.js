f7ed182bcd60466db6379ff799b9e6adaeb8a14e
li1125435097 <3162853966@qq.com>
2021-09-03 17:00:34 +0800

    add manage user

--- src/routes/list.js
+++ src/routes/list.js
@@ -1,7 +1,17 @@
 const express = require('express')
 const router = express.Router()
 const fileDbHandle = require('../../common/fileDbHandle.js').default
+const userHandle = new fileDbHandle('user',['user','pwd','name','tel','status'])
 const handle = new fileDbHandle('webPage',['href','img','name','type','doc'])
+
+router.use(async (req,res,next)=>{
+	let user = req.signedCookies.user
+	let users = await userHandle.getData()
+	console.log()
+	let manager = users.some(v=>{return (v.user==user && v.status ==100)})
+	if(manager || req.url == '/getdata'){next()}
+	else{		return res.send('你没有管理权限')		}
+})
 
 router.get('/getdata',async (req,res)=>{
 	let result = await handle.getData()
